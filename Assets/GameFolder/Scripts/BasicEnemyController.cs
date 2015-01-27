d7d72ec543cbabfe0a8c39d2fc131f0655b5a174
AldenHiggins <johnaldenhiggins@gmail.com>
2015-01-27 16:26:19 -0600

    Added zombie animations and character and beginning of animation implmenetation

---
+++ Assets/GameFolder/Scripts/BasicEnemyController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemyController : MonoBehaviour {
+	public GameObject player;
+
+	private Vector3 velocity;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		velocity = player.transform.position - transform.position;
+		velocity.y = 0.0f;
+		if (velocity.magnitude > 2)
+		{
+			velocity.Normalize ();
+			velocity *= .02f;
+			transform.position += velocity;
+		}
+	}
+}
