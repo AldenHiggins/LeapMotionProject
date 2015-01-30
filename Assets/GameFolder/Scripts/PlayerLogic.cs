fbca2c6acac10843dcf544cef041e77afad4d95a
Hadi Tabani <haditabani5@gmail.com>
2015-01-30 13:31:27 -0800

    Created Defensive Mode.

--- Assets/GameFolder/Scripts/PlayerLogic.cs
+++ Assets/GameFolder/Scripts/PlayerLogic.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class PlayerLogic : MonoBehaviour 
 {
 	public GameLogic game;
+	public bool isDefensivePlayer;
 
 	// Use this for initialization
 	void Start () 
 	{
+		if (isDefensivePlayer) {
+			transform.position = new Vector3 (55f, 130f, 0f);
+			//transform.localScale += Vector3(0.1,0,0);
+		}
+
 	}
 	
 	// Update is called once per frame
