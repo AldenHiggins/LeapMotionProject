42116944f3e6ce7ba79a36e5946ba42f22526efc
Alden Higgins <johnaldenhiggins@gmail.com>
2015-02-04 16:42:42 -0600

    Enemy physics continued

---
+++ Assets/GameFolder/Scripts/DamageInRadius.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageInRadius : MonoBehaviour 
+{
+	public int damage;
+	public float radius;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		print ("Damaging in radius!!!!");
+		Collider[] nearbyObjects = Physics.OverlapSphere (transform.position, radius);
+
+		for (int i = 0; i < nearbyObjects.Length; i++)
+		{
+			if (nearbyObjects[i].transform.childCount > 0)
+			{
+				BasicEnemyController enemy = (BasicEnemyController) nearbyObjects[i].transform.GetChild(0).gameObject.GetComponent(typeof(BasicEnemyController));
+				if (enemy != null)
+				{
+					print ("Radius dealing damage");
+					enemy.dealDamage(damage);
+				}
+			}
+
+		}
+	}
+}
