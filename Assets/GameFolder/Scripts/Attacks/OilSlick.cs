a33f553c3ba19416603740e4c35347638ecdab5b
Hadi Tabani <haditabani5@gmail.com>
2015-02-22 18:11:49 -0600

    Oil slick explodes and affects enemy.

--- Assets/GameFolder/Scripts/Attacks/OilSlick.cs
+++ Assets/GameFolder/Scripts/Attacks/OilSlick.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class OilSlick : MonoBehaviour {
 
+	public GameObject boom;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnTriggerEnter(Collider other) {
+		if (other.name == "NewFireball(Clone)") {
+//			print(other.name + " collided with oil slick.");
+			Vector3 boomPos = transform.position + new Vector3(0f,1f,0f);
+			Instantiate (boom, boomPos, Quaternion.identity);
+			Destroy(gameObject);
+		}
+
 	}
 }
