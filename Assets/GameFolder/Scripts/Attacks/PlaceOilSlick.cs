724d0d513e0dced9e4bccaf729480f430f122d13
Hadi Tabani <haditabani5@gmail.com>
2015-03-09 15:57:08 -0500

    Added the turret placement scripts. Have to test them.

--- Assets/GameFolder/Scripts/Attacks/PlaceOilSlick.cs
+++ Assets/GameFolder/Scripts/Attacks/PlaceOilSlick.cs
@@ -3,14 +3,30 @@ using System.Collections;
 
 public class PlaceOilSlick : AAttack
 {
-	public override void chargingFunction(HandModel[] hands){}
+	public DefensiveAbilities defense;
+	private GameLogic game;
+	private GameObject oilSlick;
+
+
+	public override void chargingFunction(HandModel[] hands){
+		oilSlick = (GameObject)Instantiate (defense.oilSlick);
+	}
 	
 	public override void chargedFunction(HandModel[] hands){}
 	
-	public override void releaseFunction(HandModel[] hands){}
+	public override void releaseFunction(HandModel[] hands){
+	}
 	
-	public override void holdGestureFunction(HandModel[] hands){}
+	public override void holdGestureFunction(HandModel[] hands){
+		RaycastHit hit = game.getRayHit();
+		oilSlick.transform.position = hit.point;
+	}
 	
-	public override void inactiveFunction(HandModel[] hands){}
+	public override void inactiveFunction(HandModel[] hands){
+		RaycastHit hit = game.getRayHit();
+		oilSlick.transform.position = hit.point;
+		OilSlick oilSlickScript = (OilSlick)oilSlick.GetComponent (typeof(OilSlick));
+		oilSlickScript.enabled = true;
+	}
 }
 	
