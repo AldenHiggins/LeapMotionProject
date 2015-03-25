b9ed09c554bccf682a7466ce9347537ef36ead26
Hadi Tabani <haditabani5@gmail.com>
2015-03-25 15:05:32 -0500

    Added the placement stuff.

--- Assets/GameFolder/Scripts/Attacks/PlaceOilSlick.cs
+++ Assets/GameFolder/Scripts/Attacks/PlaceOilSlick.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class PlaceOilSlick : AAttack
 {
-	public GameObject thisCamera;
 	public GameObject oilSlick;
 	public GameObject oilSlickPending;
 	private bool isInstantiated = false;
@@ -20,7 +19,7 @@ public class PlaceOilSlick : AAttack
 	public override void releaseFunction(HandModel[] hands){
 		if (defense.getNumSlicksLeft () > 0) {
 			GameObject oilSlickFinal = (GameObject)Instantiate (oilSlick);
-			oilSlickFinal.transform.position = getRayHit().point;
+			oilSlickFinal.transform.position = defense.getRayHit().point;
 			Destroy (createdOilSlick);
 			OilSlick oilSlickScript = (OilSlick)oilSlickFinal.GetComponent (typeof(OilSlick));
 			oilSlickScript.enabled = true;
@@ -28,29 +27,20 @@ public class PlaceOilSlick : AAttack
 			defense.slickUsed();
 		}
 	}
-	
+
 	public override void holdGestureFunction(HandModel[] hands){
 		if (defense.getNumSlicksLeft () > 0) {
 			if (!isInstantiated) {
 				createdOilSlick = (GameObject)Instantiate (oilSlickPending);
 				isInstantiated = true;
 			}
-			createdOilSlick.transform.position = getRayHit().point;
+			createdOilSlick.transform.position = defense.getRayHit().point;
 		}
 	}
 	
 	public override void inactiveFunction(HandModel[] hands){
-	}
-
-	private RaycastHit getRayHit()
-	{
-		int maskOne = 1 << 10;
-		int maskTwo = 1 << 11;
-		int mask = maskOne | maskTwo;
-		Ray ray = new Ray (thisCamera.transform.position, thisCamera.transform.forward);
-		RaycastHit hit;
-		Physics.Raycast (ray, out hit, 100f, mask);
-		return hit;
+		isInstantiated = false;
+		Destroy (createdOilSlick);
 	}
 }
 	
