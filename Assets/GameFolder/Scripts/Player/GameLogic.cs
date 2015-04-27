5a41a8388c9f6e32d1c62010939be2648057e890
Alden Higgins <johnaldenhiggins@gmail.com>
2015-04-27 15:55:27 -0500

    Fixed zombie deaths and integrated win/loss music

--- Assets/GameFolder/Scripts/Player/GameLogic.cs
+++ Assets/GameFolder/Scripts/Player/GameLogic.cs
@@ -331,6 +331,8 @@ public class GameLogic : MonoBehaviour
 	{
 		hmdMovement.enabled = false;
 		mainGameMusic.Pause ();
+		defensivePhaseMusic.Pause();
+		defensivePhaseMusic.mute = true;
 		StopCoroutine (roundFunction ());
 //		endRoundScreen.disableUI();
 		endRoundScreen.enabled = false;
