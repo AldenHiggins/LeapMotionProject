a12f8532d207a187a1f001ff0c5610d2e26222cc
Alden Higgins <johnaldenhiggins@gmail.com>
2015-05-05 00:13:42 -0500

    Added electric fences, changed the rounds to be more difficult, and updated the defensive UI to include the new abilities

--- Assets/GameFolder/Scripts/Utility/DamageInRadius.cs
+++ Assets/GameFolder/Scripts/Utility/DamageInRadius.cs
@@ -5,14 +5,38 @@ public class DamageInRadius : MonoBehaviour
 {
 	public int damage;
 	public float radius;
+	public float interval;
 
 
 	// Use this for initialization
 	void Start ()
 	{
-//		print ("Damaging in radius!!!!");
-		Collider[] nearbyObjects = Physics.OverlapSphere (transform.position, radius);
 
+
+//		print ("Damaging in radius!!!!");
+		dealDamageInRadius ();
+		StartCoroutine (damageOverInterval ());
+	}
+
+
+	IEnumerator damageOverInterval()
+	{
+		if (interval == 0)
+		{
+			return true;
+		}
+
+		while (true)
+		{
+			yield return new WaitForSeconds(interval);
+			dealDamageInRadius();
+		}
+	}
+
+	void dealDamageInRadius()
+	{
+		Collider[] nearbyObjects = Physics.OverlapSphere (transform.position, radius);
+		
 		for (int i = 0; i < nearbyObjects.Length; i++)
 		{
 			if (nearbyObjects[i].transform.childCount > 0)
@@ -20,11 +44,11 @@ public class DamageInRadius : MonoBehaviour
 				BasicEnemyController enemy = (BasicEnemyController) nearbyObjects[i].gameObject.GetComponent(typeof(BasicEnemyController));
 				if (enemy != null)
 				{
-//					print ("Radius dealing damage");
+					//					print ("Radius dealing damage");
 					enemy.dealDamage(damage);
 				}
 			}
-
+			
 		}
 	}
 }
