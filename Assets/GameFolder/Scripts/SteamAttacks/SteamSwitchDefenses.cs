f2699ad829386a6cf9d997f3700e1dd6768e5acc
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-08-27 12:47:34 -0700

    Got rid of unused attack container in the switch defenses attack...now all of the defensive options are included in the defense attack itself

--- Assets/GameFolder/Scripts/SteamAttacks/SteamSwitchDefenses.cs
+++ Assets/GameFolder/Scripts/SteamAttacks/SteamSwitchDefenses.cs
@@ -3,31 +3,16 @@ using UnityEngine;
 
 public class SteamSwitchDefenses : SteamAttacks
 {
-	private int currentAttackIndex = 0;
-	public GameObject attackChoices;
 	public PlaceDefenseSteam defensiveAttack;
 
-    public override void inactiveFunction()
-    {
-
-    }
+    public override void inactiveFunction(){}
 
     public override void releaseFunction(OVRInput.Controller hand)
     {
-        currentAttackIndex++;
-        if (currentAttackIndex >= attackChoices.transform.childCount)
-        {
-            currentAttackIndex = 0;
-        }
-
         defensiveAttack.switchDefense();
     }
 
-    public override void holdFunction(OVRInput.Controller hand)
-    {
-
-    }
-
+    public override void holdFunction(OVRInput.Controller hand){}
 }
 
 
