e17352a51a2d548b22bc3cb40d4ed75f932ad307
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-08-29 18:25:58 -0700

    Cleaned up the attack particle script a tiny bit and gave it the capability of spawning a particle on collision with an enemy

--- Assets/GameFolder/Scripts/Attacks/AttackParticle.cs
+++ Assets/GameFolder/Scripts/Attacks/AttackParticle.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AttackParticle : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject impactParticle;
+
+    [SerializeField]
+    private int attackDamage = 20;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -18,15 +25,32 @@ public class AttackParticle : MonoBehaviour
 	void OnParticleCollision(GameObject other)
     {
 		Rigidbody body = other.GetComponent<Rigidbody>();
-		if (body)
+        if (body == null)
         {
-            BasicEnemyController foundEnemy = body.gameObject.GetComponent<BasicEnemyController>();
-            
-            if (foundEnemy != null)
-            {
-                foundEnemy.dealDamage(20);
-                Destroy(gameObject);
-            }
-		}
-	}
+            return;
+        }
+
+        BasicEnemyController foundEnemy = body.gameObject.GetComponent<BasicEnemyController>();
+        if (foundEnemy == null)
+        {
+            return;
+        }
+
+        // If we have collided with an enemy deal the appropriate amount of damage
+        foundEnemy.dealDamage(attackDamage);
+
+        // Get access to the collision data
+        List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+        GetComponent<ParticleSystem>().GetCollisionEvents(other, collisionEvents);
+
+        // Spawn an impact particle if necessary
+        if (impactParticle != null)
+        {
+            GameObject newImpactParticle = Instantiate(impactParticle, collisionEvents[0].intersection, Quaternion.identity);
+            newImpactParticle.SetActive(true);
+        }
+
+        // Finally destroy this particle
+        Destroy(gameObject);
+    }
 }
