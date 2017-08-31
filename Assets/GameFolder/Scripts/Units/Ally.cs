0b3ca05cf93ef3f845ce2d07149757e559623672
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-08-30 18:02:35 -0700

    Simple new script for allied units

---
+++ Assets/GameFolder/Scripts/Units/Ally.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ally : AUnit
+{
+    override protected void initializeUnit()
+    {
+        enemySearchLayer = 1 << 8;
+        isAlly = true;
+    }
+}
