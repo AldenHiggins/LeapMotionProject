0f157e665cfc5685acaad1c7c7882e05981422cf
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-08-31 17:00:36 -0700

    Added a new DefensiveGrid script in preparation to limit where the user can place defensive objects into defined squares

---
+++ Assets/GameFolder/Scripts/Defenses/DefensiveGrid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefensiveGrid : MonoBehaviour
+{
+    [SerializeField]
+    private int numColumns = 10;
+
+    [SerializeField]
+    private int numRows = 10;
+
+    [SerializeField]
+    private GameObject gridLine;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Generate all of the columns
+        for (int colIndex = 0; colIndex < numColumns; colIndex++)
+        {
+            float zPosition = -.5f + ((1.0f / (numColumns - 1)) * colIndex);
+            GameObject newGridLine = Instantiate(gridLine, transform);
+            newGridLine.transform.localPosition = new Vector3(0.0f, 1.0f, zPosition);
+        }
+
+        // Generate all of the rows
+        for (int rowIndex = 0; rowIndex < numRows; rowIndex++)
+        {
+            float xPosition = -.5f + ((1.0f / (numColumns - 1)) * rowIndex);
+            GameObject newGridLine = Instantiate(gridLine, transform);
+            newGridLine.transform.localRotation = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+            newGridLine.transform.localPosition = new Vector3(xPosition, 1.0f, 0.0f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
