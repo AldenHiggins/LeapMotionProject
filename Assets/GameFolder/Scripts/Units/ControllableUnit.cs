a233f22cd951ea8a290828de6dea555c5328ada3
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-09-06 18:41:27 -0700

    Now move based on the frame rate as well.  The unit now rotates towards its movement vector, only move when it's close enough to the movement rotation, and does a running animation.

--- Assets/GameFolder/Scripts/Units/ControllableUnit.cs
+++ Assets/GameFolder/Scripts/Units/ControllableUnit.cs
@@ -8,6 +8,8 @@ public class ControllableUnit : MonoBehaviour, IUnit
 {
     // CONTROLLER
     private CharacterController controller;
+    // ANIMATOR
+    private Animator anim;
     // HEALTH
     [SerializeField]
     private int startingHealth = 100;
@@ -15,6 +17,10 @@ public class ControllableUnit : MonoBehaviour, IUnit
     // MOVEMENT
     [SerializeField]
     private float walkSpeed = .1f;
+    [SerializeField]
+    private float turnSpeed = .1f;
+    [SerializeField]
+    private float movementAngle = 10.0f;
     // IS ALIVE
     private bool isDying;
 
@@ -23,15 +29,38 @@ public class ControllableUnit : MonoBehaviour, IUnit
     {
         currentHealth = startingHealth;
         controller = GetComponent<CharacterController>();
+        anim = transform.GetChild(0).GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Take in input from the player
         Vector2 leftInput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+        // Find the movement vector of the unit
         Vector3 movementVector = new Vector3(leftInput.x, 0.0f, leftInput.y);
-        movementVector *= walkSpeed;
-        controller.Move(movementVector);
+        // Don't do anything if we haven't received any input
+        if (movementVector == Vector3.zero)
+        {
+            anim.SetBool("Running", false);
+            return;
+        }
+        // Rotate the unit
+        Quaternion targetRot = Quaternion.LookRotation(movementVector);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * turnSpeed);
+
+        // Move the unit if it's sufficiently pointed in the direction of its intended movement
+        float rotationAngle = Quaternion.Angle(targetRot, transform.rotation);
+        if (rotationAngle <= movementAngle)
+        {
+            anim.SetBool("Running", true);
+            controller.Move(movementVector * walkSpeed * Time.deltaTime);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
     }
 
     public void installDeathListener(Action onDeathCallback) { }
