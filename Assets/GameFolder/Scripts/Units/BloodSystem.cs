2c3d613daaa658601fde168d1de1a78c5bd08952
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-09-10 14:04:31 -0700

    Started implementing a system to emit blood particles when a unit is damaged

---
+++ Assets/GameFolder/Scripts/Units/BloodSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodSystem : MonoBehaviour
+{
+    private IUnit parentUnit;
+
+    [SerializeField]
+    private GameObject bloodParticle;
+
+    private ParticleSystem subParticle;
+
+    // Use this for initialization
+    void Start ()
+    {
+        parentUnit = transform.parent.GetComponent<IUnit>();
+        if (parentUnit == null)
+        {
+            Debug.LogError("Health bar needs to be the child of an IUnit!");
+            return;
+        }
+
+        parentUnit.installDamageListener(onDamageTaken);
+
+        subParticle = transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
+    }
+
+    void onDamageTaken(int damageTaken)
+    {
+        Debug.Log("Simulating blood!");
+        subParticle.Stop();
+        subParticle.Play();
+    }
+}
