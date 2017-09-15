871482d1afa02ae7f29157a167236473cd53682c
AldenHiggins <johnaldenhiggins@gmail.com>
2017-09-15 15:33:56 -0700

    Simple utility scripts used to load in the room level and attach it to the correct game object on load

---
+++ Assets/GameFolder/Scripts/Utility/AttachToParentOnLoad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachToParentOnLoad : MonoBehaviour
+{ 
+	// Use this for initialization
+	void Start ()
+    {
+        transform.parent = GetObjects.getGame().gameObject.transform.parent;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/GameFolder/Scripts/Utility/LoadLevelOnStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevelOnStart : MonoBehaviour
+{
+    [SerializeField]
+    private string levelName;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Application.LoadLevelAdditive(levelName);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
