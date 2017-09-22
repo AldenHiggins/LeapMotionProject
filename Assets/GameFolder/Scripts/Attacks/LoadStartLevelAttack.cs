e0addcc942a953bb0e6d6a2c575429ba943b7153
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-09-22 14:16:22 -0700

    New attack that allows the user to return to the level select level.

---
+++ Assets/GameFolder/Scripts/Attacks/LoadStartLevelAttack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadStartLevelAttack : AAttack
+{
+    public override void inactiveFunction() { }
+
+    public override void releaseFunctionConcrete(Vector3 localPos, Vector3 worldPos, Quaternion localRot, Quaternion worldRot)
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(2).name);
+        SceneManager.LoadScene("LevelSelectTest", LoadSceneMode.Additive);
+        GetObjects.getMovingObjectsContainer().transform.position = Vector3.zero;
+        return;
+    }
+
+    public override void holdFunctionConcrete(Vector3 localPos, Vector3 worldPos, Quaternion localRot, Quaternion worldRot) { }
+}
