0a80d6cba788a83b0ed450ee1c0c71ea7953537f
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-09-22 13:58:02 -0700

    Simple script to reset the transform of the moving objects container when I load up the level select level

---
+++ Assets/GameFolder/Scripts/Utility/ResetMovingObjectsOnLoad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetMovingObjectsOnLoad : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetObjects.getMovingObjectsContainer().transform.position = Vector3.zero;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
