67443f11dda0b1fe192b675cc833ae849059a4cd
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-09-22 13:58:26 -0700

    Simple script to load a level when you run into one of the triggers in the level select area

---
+++ Assets/GameFolder/Scripts/Utility/LoadLevelOnTriggerEnter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelOnTriggerEnter : MonoBehaviour
+{
+    [SerializeField]
+    private string levelName;
+
+    [SerializeField]
+    private string unloadLevel;
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(levelName, LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync(unloadLevel);
+    }
+}
