94db91779351dcf0da126ad178db4ede660c5d18
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-09-24 17:21:51 -0700

    Added a new corpse explosion ability

--- Assets/GameFolder/Scripts/Attacks/SpawnSkeletonAttack.cs
+++ Assets/GameFolder/Scripts/Attacks/SpawnSkeletonAttack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class SpawnSkeletonAttack : AAttack
 {
 	public GameObject zombiePrefab;
-    public PlayerLogic player;
+    private PlayerLogic player;
 
     private void Start()
     {
