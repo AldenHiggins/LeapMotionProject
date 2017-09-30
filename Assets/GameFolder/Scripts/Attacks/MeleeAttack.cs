53a6b2f135722d1d3fc8a21d99dffb6230a908f4
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-09-30 12:40:19 -0700

    Made the melee and shadow ball attack activate on the hold function instead of on release.  This allows me to have a consistent interface for all the controls where holding down a button/trigger calls the hold function and the release function only happens when the button/trigger is released.  Waiting to activate the attaks on release just wasn't responsive enough.

--- Assets/GameFolder/Scripts/Attacks/MeleeAttack.cs
+++ Assets/GameFolder/Scripts/Attacks/MeleeAttack.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 
 public class MeleeAttack : AAttack
 {
+    private bool hasAttacked = false;
+
     public override void inactiveFunction() { }
 
     public override void releaseFunctionConcrete(Vector3 localPos, Vector3 worldPos, Quaternion localRot, Quaternion worldRot)
     {
-        GetObjects.instance.getControllableUnit().doMeleeAttack();
+        hasAttacked = false;
     }
 
-    public override void holdFunctionConcrete(Vector3 localPos, Vector3 worldPos, Quaternion localRot, Quaternion worldRot) { }
+    public override void holdFunctionConcrete(Vector3 localPos, Vector3 worldPos, Quaternion localRot, Quaternion worldRot)
+    {
+        if (!hasAttacked)
+        {
+            GetObjects.instance.getControllableUnit().doMeleeAttack();
+            hasAttacked = true;
+        }
+    }
 }
 
 
--- Assets/GameFolder/Scripts/Attacks/ShadowBallAttack.cs
+++ Assets/GameFolder/Scripts/Attacks/ShadowBallAttack.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 
 public class ShadowBallAttack : AAttack
 {
+    private bool hasAttacked = false;
+
     public override void inactiveFunction(){}
 
     public override void releaseFunctionConcrete(Vector3 localPos, Vector3 worldPos, Quaternion localRot, Quaternion worldRot)
     {
-        GetObjects.instance.getControllableUnit().doShadowBallAttack();
+        hasAttacked = false;
     }
 
-    public override void holdFunctionConcrete(Vector3 localPos, Vector3 worldPos, Quaternion localRot, Quaternion worldRot) { }
+    public override void holdFunctionConcrete(Vector3 localPos, Vector3 worldPos, Quaternion localRot, Quaternion worldRot)
+    {
+        if (!hasAttacked)
+        {
+            GetObjects.instance.getControllableUnit().doShadowBallAttack();
+            hasAttacked = true;
+        }
+    }
 }
 
 
