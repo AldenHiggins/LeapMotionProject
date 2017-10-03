eb91549e7004e4b6987e2fb3e0e5495da1e11d74
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-10-03 13:40:58 -0700

    New attack that spawns an object at a pointe that you've raycasted to on the ground plane.  Could be used for lots of different things but for now I just spawn the spikes that I just added.

---
+++ Assets/GameFolder/Scripts/Attacks/SpawnObjectAtRaycastAttack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectAtRaycastAttack : AAttack
+{
+    public GameObject objectToSpawn;
+
+    private int groundLayer = 1 << 10;
+
+    private PlayerLogic player;
+
+    private void Start()
+    {
+        player = GetObjects.instance.getPlayer();
+    }
+
+    public override void inactiveFunction() { }
+
+    public override void releaseFunctionConcrete(Vector3 localPos, Vector3 worldPos, Quaternion localRot, Quaternion worldRot)
+    {
+        // Hide the pointer
+        player.hidePointers();
+
+        // Raycast onto the defensive grid
+        RaycastHit hit;
+        Physics.Raycast(worldPos, (worldRot * Vector3.forward), out hit, 100.0f, groundLayer);
+
+        // Make sure the raycast hit the defensive grid
+        if (hit.collider == null)
+        {
+            return;
+        }
+        if (hit.collider.gameObject == null)
+        {
+            return;
+        }
+
+        Instantiate(objectToSpawn, hit.point, Quaternion.identity, GetObjects.instance.getAttackParticleContainer());
+    }
+
+    public override void holdFunctionConcrete(Vector3 localPos, Vector3 worldPos, Quaternion localRot, Quaternion worldRot)
+    {
+        player.showPointer(false);
+    }
+}
