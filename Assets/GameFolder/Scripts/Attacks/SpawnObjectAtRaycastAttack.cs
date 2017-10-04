d9c403a4b710df9ef29e60a9cc681a538311db2a
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-10-04 13:08:34 -0700

    The spike attack now has an indicator where it is going to be cast and also has a cast range.  If the user points outside of the cast range then the spell is cast at max range along the vector where the user is pointing.  Works pretty well right now.  But as it is all of this functionality sits within the SpawnObjectAtRaycastAttack and repeating it for things like summon skeleton and corpse explosion seems wrong.

--- Assets/GameFolder/Scripts/Attacks/SpawnObjectAtRaycastAttack.cs
+++ Assets/GameFolder/Scripts/Attacks/SpawnObjectAtRaycastAttack.cs
@@ -6,6 +6,10 @@ public class SpawnObjectAtRaycastAttack : AAttack
 {
     public GameObject objectToSpawn;
 
+    public GameObject aoeIndicator;
+
+    public float castRange;
+
     private int groundLayer = 1 << 10;
 
     private PlayerLogic player;
@@ -15,14 +19,17 @@ public class SpawnObjectAtRaycastAttack : AAttack
     private void Start()
     {
         player = GetObjects.instance.getPlayer();
+        aoeIndicator = Instantiate(aoeIndicator, GetObjects.instance.getAttackContainer());
+        aoeIndicator.SetActive(false);
     }
 
     public override void inactiveFunction() { }
 
     public override void releaseFunctionConcrete(Vector3 localPos, Vector3 worldPos, Quaternion localRot, Quaternion worldRot)
     {
-        // Hide the pointer
+        // Hide the pointer and aoe indicator
         player.hidePointers();
+        aoeIndicator.SetActive(false);
 
         // Get access to the unit
         ControllableUnit unit = GetObjects.instance.getControllableUnit();
@@ -31,21 +38,11 @@ public class SpawnObjectAtRaycastAttack : AAttack
         if (unit.getAttacking()) return;
 
         // Raycast onto the defensive grid
-        RaycastHit hit;
-        Physics.Raycast(worldPos, (worldRot * Vector3.forward), out hit, 100.0f, groundLayer);
-
-        // Make sure the raycast hit the defensive grid
-        if (hit.collider == null)
-        {
-            return;
-        }
-        if (hit.collider.gameObject == null)
-        {
-            return;
-        }
+        Vector3 raycastPoint;
+        if (!raycastTheGround(worldPos, worldRot, out raycastPoint)) return;
 
         // Set the spawn position for the object to be our raycast point
-        currentSpawnPosition = hit.point;
+        currentSpawnPosition = raycastPoint;
         // Have the unit face the point we're casting towards
         unit.transform.LookAt(currentSpawnPosition, Vector3.up);
         // Have the unit cast this spell
@@ -55,10 +52,50 @@ public class SpawnObjectAtRaycastAttack : AAttack
     public override void holdFunctionConcrete(Vector3 localPos, Vector3 worldPos, Quaternion localRot, Quaternion worldRot)
     {
         player.showPointer(false);
+        // Raycast onto the defensive grid to place a spell cast indicator
+        Vector3 raycastPoint;
+        if (!raycastTheGround(worldPos, worldRot, out raycastPoint))
+        {
+            aoeIndicator.SetActive(false);
+            return;
+        }
+
+        aoeIndicator.transform.position = raycastPoint + new Vector3(0.0f, 0.05f, 0.0f);
+        aoeIndicator.SetActive(true);
     }
 
     private void createObjectOnCast()
     {
         Instantiate(objectToSpawn, currentSpawnPosition, Quaternion.identity, GetObjects.instance.getAttackParticleContainer());
+    }
+
+    private bool raycastTheGround(Vector3 worldPos, Quaternion worldRot, out Vector3 raycastPoint)
+    {
+        RaycastHit hit;
+        Physics.Raycast(worldPos, (worldRot * Vector3.forward), out hit, 100.0f, groundLayer);
+
+        // Make sure the raycast hit the defensive grid
+        if (hit.collider == null)
+        {
+            raycastPoint = Vector3.zero;
+            return false;
+        }
+        if (hit.collider.gameObject == null)
+        {
+            raycastPoint = Vector3.zero;
+            return false;
+        }
+
+        // Check if the cast is within range, if so just return the raycast point
+        Vector3 unitPosition = GetObjects.instance.getControllableUnit().transform.position;
+        Vector3 vectorToRaycastPoint = hit.point - unitPosition;
+        if (vectorToRaycastPoint.magnitude < castRange)
+        {
+            raycastPoint = hit.point;
+            return true;
+        }
+        // If the cast is out of range show the max range spot that the spell can reach
+        raycastPoint = unitPosition + (vectorToRaycastPoint.normalized * castRange);
+        return true;
     }
 }
